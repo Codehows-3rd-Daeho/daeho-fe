4b75cb0cc52301545f6cff675d17ad75e34e7120
chwolog1115 <chwolog1115@gmail.com>
2025-12-10 14:10:20 +0900

    11

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Daeho",
+        short_name: "dh",
+        description: "Daeho",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icon152.png",
+            sizes: "152x152",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+
   server: {
     host: "localhost",
     proxy: {
